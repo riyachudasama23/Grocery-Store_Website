e21bf351181a7dc6b293381b6c2def92a4126cc0
riyachudasama23 <114990906+riyachudasama23@users.noreply.github.com>
2023-06-18 01:33:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,31 @@ let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
 }
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+}
+
+let navBar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navBar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navBar.classList.remove('active');
+}
